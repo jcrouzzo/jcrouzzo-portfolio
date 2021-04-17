3bbac5c1ba7d2931b4f8f8a5513895217f60c75c
Justin Rouzzo <jcrouzzo@gmail.com>
2021-04-17 06:10:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showmenu()
+{
+    var x = document.getElementById('menuContent');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
